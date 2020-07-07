24af21a134a21adcd3e7dfd7ddd30b28564c2e1b
Ivan #1 <ivan_1@IvanHanak.local>
2020-07-07 21:38:10 +0200

    Added support for nulls, undefineds and fixing not rendering zero

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 export default (...args) => {
-    return args.filter(item => !!item).join(" ");
+    return args.filter(item => {
+        if(typeof item === "undefined") {
+            return false;
+        } else if(item === null) {
+            return false;
+        } else if ([true, false].includes(item)) {
+            return false;
+        }
+
+        return item.toString().trim().length > 0
+    }).join(" ");
 }
